3d03290e83b0149d0dbdba46bce0636bb1286887
dailian <chaojidailian@gmail.com>
2023-04-02 10:57:03 +0800

    fix(.):修复一些bug

--- my-vue3-project/uniCloud-aliyun/cloudfunctions/banners-get/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const db = uniCloud.database()
-
-exports.main = async (event, context) => {
-  // 获取 `user` 集合的引用
-  const result = await db.collection('banners').get()
-
-  return result
-};
